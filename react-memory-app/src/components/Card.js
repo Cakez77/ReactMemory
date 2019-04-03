ce2cb9ea5a9db94ab0eb1779f05fc3587eed9092
Bernd Eissing <bernd@bernds-mbp.speedport.ip>
2019-04-03 14:11:41 +0200

    added on Click and state change to Card component

--- react-memory-app/src/components/Card.js
+++ react-memory-app/src/components/Card.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 
 
 class Card extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { clicked: false }
+  }
+
+  onClick = () => {
+    this.setState({
+      ...this.state,
+      clicked: !this.state.clicked
+    })
+  }
+
   render() {
-    return (
-    <div style={{height: 200, width: 200, backgroundColor: '#555555', border: '2px solid #000000'}}>
+    return !this.state.clicked ? (
+    <div onClick={this.onClick} style={{height: 200, width: 200, backgroundColor: '#555555'}}>
+      {"Card"}
+    </div>
+    ) : (
+      <div onClick={this.onClick} style={{height: 200, width: 200, backgroundColor: '#555555', border: '2px solid #000000'}}>
       {"Card"}
     </div>
     )
